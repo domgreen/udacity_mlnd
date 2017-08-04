5435e1c9fed2d700a11436519aeb98a82e3ce0f0
Dominic Green <dominicgreen1@gmail.com>
2017-08-04 16:55:22 +0100

    implementing naive bayes theorm for words in speech

--- spam.py
+++ spam.py
@@ -50,3 +50,63 @@ p_pos_no_diabetes = 0.1
 # P('D|Pos)
 p_no_diabetes_pos = (p_no_diabetes * p_pos_no_diabetes) / p_pos
 print('The probability of NOT having diabetes given a positive test result: {}'.format(p_no_diabetes_pos))
+
+print('NAIVE BAYES THEORM')
+
+'''
+Instructions: Compute the probability of the words 'freedom' and 'immigration' being said in a speech, or
+P(F,I).
+
+The first step is multiplying the probabilities of Jill Stein giving a speech with her individual
+probabilities of saying the words 'freedom' and 'immigration'. Store this in a variable called p_j_text
+'''
+
+# P(J)
+p_j = 0.5
+
+# P(F|J)
+p_j_f = 0.1
+
+# P(I|J)
+p_j_i = 0.1
+
+p_j_text = p_j * p_j_f * p_j_i
+print(p_j_text)
+
+'''
+The second step is multiplying the probabilities of Gary Johnson giving a speech with his individual
+probabilities of saying the words 'freedom' and 'immigration'. Store this in a variable called p_g_text
+'''
+
+# P(G)
+p_g = 0.5
+
+# P(F|G)
+p_g_f = 0.7
+
+# P(I|G)
+p_g_i = 0.2
+
+p_g_text = p_g * p_g_f * p_g_i
+
+print(p_g_text)
+
+'''
+The third step is to add both of these probabilities and you will get P(F,I).
+'''
+
+p_f_i = p_g_text + p_j_text
+print('Probability that words Freedom and Immigration are said: {}'.format(p_f_i))
+
+'''
+Instructions:
+Compute P(J|F,I) using the formula P(J|F,I) = (P(J) * P(F|J) * P(I|J)) / P(F,I) and store it in a variable p_j_fi
+'''
+
+# P(J|F,I)
+p_j_fi = (p_j_text) / p_f_i
+print('The probability of Jill given words Freedom and Imigration: {}'.format(p_j_fi))
+
+# P(G|F,I)
+p_g_fi = (p_g_text) / p_f_i
+print('The probability of Gary given words Freedom and Immigration: {}'.format(p_g_fi))
