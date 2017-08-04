d16a21252d3cefedb293b97e8b867c972693cfd1
Dominic Green <dominicgreen1@gmail.com>
2017-08-04 16:32:21 +0100

    completed bayes theorm example

--- spam.py
+++ spam.py
@@ -1,23 +1,52 @@
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.cross_validation import train_test_split
 
 df = pd.read_table('~/python/udacity_mlnd/smsspamcollection/SMSSpamCollection',
                    sep='\t',
                    header=None,
                    names=['label', 'sms_message'])
 df['label'] = df.label.map({'ham': 0, 'spam': 1})
+x_train, x_test, y_train, y_test = train_test_split(df['sms_message'],
+                                                    df['label'],
+                                                    random_state=1)
 
-print(df.shape)
-print(df.head())
-
-documents = ['Hello, how are you!',
-             'Win money, win from home.',
-             'Call me now.',
-             'Hello, Call hello you tomorrow?']
+print('Number of lines total: {}'.format(df.shape[0]))
+print('Number of lines train: {}'.format(x_train.shape[0]))
+print('Number of lines test : {}'.format(x_test.shape[0]))
 
 count_vector = CountVectorizer()
-count_vector.fit(documents)
-doc_array = count_vector.transform(documents).toarray()
-frequency_matrix = pd.DataFrame(doc_array,
-                                columns=count_vector.get_feature_names())
-print(frequency_matrix)
+# BoW - create the vocabulary and then transform it into a count of each word
+training_data = count_vector.fit_transform(x_train)
+testing_data = count_vector.transform(x_test)
+
+print('BAYES THEORM')
+
+# P(D)
+p_diabetes = 0.01
+
+# P('D)
+p_no_diabetes = 0.99
+
+# Sensitivity or P(Pos|D)
+p_pos_diabetes = 0.9
+
+# Specificity or P(Neg|'D)
+p_neg_no_diabetes = 0.9
+
+# P(Pos)
+p_pos = (p_diabetes * p_pos_diabetes) + (p_no_diabetes * (1 - p_neg_no_diabetes))
+
+print('The probability of getting a positive test result P(Pos) is: {}'.format(p_pos))
+
+# P(D|Pos)
+p_diabetes_pos = (p_diabetes * p_pos_diabetes) / p_pos
+
+print('The probability of having diabetes given a positive test result: {}'.format(p_diabetes_pos))
+
+# P(Pos|'D)
+p_pos_no_diabetes = 0.1
+
+# P('D|Pos)
+p_no_diabetes_pos = (p_no_diabetes * p_pos_no_diabetes) / p_pos
+print('The probability of NOT having diabetes given a positive test result: {}'.format(p_no_diabetes_pos))
